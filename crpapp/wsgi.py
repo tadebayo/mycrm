17015e1095e370673977dc0cf9ebabc20f982c7e
Adebayo <message4bayo@gmail.com>
2017-02-28 14:06:58 +0100

    configured static files settings

--- crpapp/wsgi.py
+++ crpapp/wsgi.py
@@ -1,14 +1,17 @@
 """
-WSGI config for crpapp project.
+WSGI config for crmapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crpapp.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crmapp.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
